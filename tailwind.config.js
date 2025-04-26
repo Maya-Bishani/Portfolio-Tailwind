0f456f8d9de424b9ced8cc2e5a114b8bd3fbe162
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2025-04-26 07:17:11 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,27 @@ export default {
   "./src/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
+    colors: {
+      brown : "#53423e",
+      lightBrown : "#645550",
+      darkBrown : "#2c2523",
+      black : "#1e1917",
+      white : "#f1e1d9",
+      cyan : "#15d1e9",
+      lightCyan : "#88e5f0",
+      darkCyan : "#009fb3",
+      orange : "#fb9718",
+      lightOrange : "#fac27b",
+      darkOrange : "#"
+    },
     extend: {
       rotate:{
         'y-180':'180deg'
       }
+    },
+    fontFamily:{
+      body: ['Josefin Sans'],
+      special: ['Roboto'],
     },
     screens:{
       'sm':'640px',
